4b5b57d36a46bbb67d118bbe81b4c60ce4efd15f
Oxmada <antoineponthieu@hotmail.fr>
2025-03-12 11:42:11 +0300

    Ajout de la récupération des données manquantes dans la fonction photographerTemplate

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +9,19 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const p1 = document.createElement('p');
+        p1.textContent = `${city}, ${country}`;
+        const p2 = document.createElement('p');
+        p2.textContent = tagline;
+        const p3 = document.createElement('p');
+        p3.textContent = `${price}€/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(p3)
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }

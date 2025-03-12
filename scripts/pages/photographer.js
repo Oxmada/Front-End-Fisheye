84b6848c90638b42d21dc61c0e547cab18aa9705
Oxmada <antoineponthieu@hotmail.fr>
2025-03-12 19:20:06 +0300

    Ajout du code pour récupérer l'ID du photographe depuis l'url

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,13 @@
-//Mettre le code JavaScript lié à la page photographer.html
+function getPhotographersId() {
+
+    // Récupère la partie query de l'url (tout ce qui se trouve après le ?)
+    const params = new URLSearchParams(window.location.search);
+
+    // Récupère la valeur du paramètre "id" et la retourne.
+    return params.get ("id");
+}
+
+const photographerId = getPhotographersId();
+
+// Vérifie si l'ID est bien récupéré
+console.log("photographer ID:", photographerId); 

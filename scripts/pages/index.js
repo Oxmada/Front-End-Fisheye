4bb8cccd8db246a050483d2d03e723862c2d2066
Oxmada <antoineponthieu@hotmail.fr>
2025-03-18 22:47:44 +0300

    Récupération des données du photographe et suppression de la balise <a> de la carte

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,53 +1,49 @@
-
 async function getPhotographers() {
-    // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-    // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+  try {
+    // Envoie une requête HTTP pour récupérer le fichier JSON.
+    const response = await fetch("../../data/photographers.json");
 
-    try {
-        // Envoie une requête HTTP pour récupérer le fichier JSON.
-        const response = await fetch("../../data/photographers.json");
-
-        // Vérifie si la requête a réussi
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP ! Status: ${response.status}`);
-        }
-
-        // Analyse et transforme la réponse JSON en objet JavaScript
-        const data = await response.json();
-
-        // Vérifiez que les données récupérées contiennent bien la clé "photographers"
-        // et que cette clé est associée à un tableau.
-        if (!data.photographers || !Array.isArray(data.photographers)) {
-            throw new Error("Les données récupérées ne contiennent pas un tableau de photographes.");
-        }
-
-        // Retourne les photographes récupérés
-        return {photographers: data.photographers};
-
-    } catch (error) {
-        // Gestion des erreurs potentielles
-        console.error("Erreur lors de la récupération des photographes:", error);
-        return {
-            photographers: []
-        };
+    // Vérifie si la requête a réussi
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP ! Status: ${response.status}`);
     }
+
+    // Analyse et transforme la réponse JSON en objet JavaScript
+    const data = await response.json();
+
+    // Vérifie que les données récupérées contiennent bien la clé "photographers"
+    // et que cette clé est associée à un tableau.
+    if (!data.photographers || !Array.isArray(data.photographers)) {
+      throw new Error(
+        "Les données récupérées ne contiennent pas un tableau de photographes."
+      );
+    }
+
+    // Retourne les photographes récupérés
+    return { photographers: data.photographers };
+  } catch (error) {
+    // Gestion des erreurs potentielles
+    console.error("Erreur lors de la récupération des photographes:", error);
+    return {
+      photographers: [],
+    };
+  }
 }
 
 async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
+  const photographersSection = document.querySelector(".photographer_section");
 
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerTemplate(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
 }
 
 async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
 }
 
 init();
-
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,44 +1,71 @@
 function getPhotographersId() {
+  // Récupère la partie query de l'url (tout ce qui se trouve après le ?)
+  const params = new URLSearchParams(window.location.search);
 
-    // Récupère la partie query de l'url (tout ce qui se trouve après le ?)
-    const params = new URLSearchParams(window.location.search);
-
-    // Récupère la valeur du paramètre "id" et la retourne.
-    return params.get ("id");
+  // Récupère la valeur du paramètre "id" et la retourne.
+  return params.get("id");
 }
 
 const photographerId = getPhotographersId();
 
 // Vérifie si l'ID est bien récupéré
-console.log("photographer ID:", photographerId); 
+console.log("photographer ID:", photographerId);
 
 async function getPhotographerData(photographerId) {
-    try {
-        // Récupère les informations du photographe et ses médias associés
-        const response = await fetch("../../data/photographers.json");
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP ! Status: ${response.status}`);
-        }
-        const data = await response.json();
-
-        // Trouve le photographe par ID
-        const photographer = data.photographers.find(photographer => photographer.id == photographerId);
-
-        // Filtre les médias associés au photographe
-        const media = data.media.filter(item => item.photographerId == photographerId);
-
-        return {
-            photographer,
-            media
-        };
-    } catch (error) {
-        console.error("Erreur lors de la récupération des données :", error);
+  try {
+    // Récupère les informations du photographe et ses médias associés
+    const response = await fetch("../../data/photographers.json");
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP ! Status: ${response.status}`);
     }
+    const data = await response.json();
+
+    // Trouve le photographe par ID
+    const photographer = data.photographers.find(
+      (photographer) => photographer.id == photographerId
+    );
+
+    // Filtre les médias associés au photographe
+    const media = data.media.filter(
+      (item) => item.photographerId == photographerId
+    );
+
+    return {
+      photographer,
+      media,
+    };
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données :", error);
+  }
 }
 
 // Récupère la promesse retournée par la fonction getPhotographerData
-getPhotographerData(photographerId).then(PhotographerData => {
+getPhotographerData(photographerId).then((PhotographerData) => {
+  //Affiche les données du photographe et ses médias
+  console.log("photographer Data:", PhotographerData);
+});
 
-    //Affiche les données du photographe et ses médias
-    console.log("photographer Data:", PhotographerData);
+getPhotographerData(photographerId).then((PhotographerData) => {
+  if (PhotographerData && PhotographerData.photographer) {
+    const photographerCard = photographerTemplate(
+      PhotographerData.photographer
+    ).getUserCardDOM();
+
+    // Sélectionne la balise <a>
+    const link = photographerCard.querySelector(".photographer-link");
+    if (link) {
+      // déplace les enfants de <a> vers le parent de <a>
+      while (link.firstChild) {
+        link.parentNode.insertBefore(link.firstChild, link);
+      }
+
+      // Supprime la balise <a> qui est maintenant vide
+      link.parentNode.removeChild(link);
+    }
+
+    const parentElement = document.querySelector(".photograph-header");
+    if (parentElement) {
+      parentElement.appendChild(photographerCard);
+    }
+  }
 });
